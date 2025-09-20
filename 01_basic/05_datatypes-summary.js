bc7d84d579e644ea04b6f13df989b55c6ce09148
Abhishek Pandey <anshabhipandey@gmail.com>
2025-09-20 06:20:07 +0000

    Stack and Heap Memory

--- 01_basic/05_datatypes-summary.js
+++ 01_basic/05_datatypes-summary.js
@@ -30,3 +30,30 @@ let myFunction = function(){
     console.log("Hello World");
     
 }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory : Memory two types ki hoti hai 
+// 1. Stack, 2. Heap 
+// Stack(Primitive) me use hota hai, Heap(Non primitive) me use hota hai.
+// Stack ke ander variable ki value ki copy milti hai.
+// Heap ke ander variable ki value ka Reference same milta hai.
+
+let myYoutubename = "hiteshchoudharydotcom";
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "abhishek@gmail.com",
+    upi: "user@ybl"
+};
+
+let userTwo = userOne;
+userTwo.email = "ansh123@gmail.com";
+
+console.log(userTwo.email);
+console.log(userOne.email);
+
